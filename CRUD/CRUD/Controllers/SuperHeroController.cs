c003cc0f1420bebf93c74fd31e8ca756e34cad54
somsakBitbucket <somsak.kaeworasan@outlook.com>
2022-02-01 17:29:49 +0700

    Add HttpGet.

---
+++ CRUD/CRUD/Controllers/SuperHeroController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+
+
+        public SuperHeroController(DataContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            List<SuperHero> result = new List<SuperHero>();
+            using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            conn.Open();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT  [Id],[Name] FROM [superherodb].[dbo].[SuperHeroes]";
+            using var reader = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+            while (await reader.ReadAsync())
+            {
+                result.Add(new SuperHero
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1)
+                });
+            }
+
+            return Ok(result);
+        }
+    }
+
+   
+}
---
+++ CRUD/CRUD/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public virtual DbSet<SuperHero> SuperHeroes { get; set; } = null!;
+    }
+}
